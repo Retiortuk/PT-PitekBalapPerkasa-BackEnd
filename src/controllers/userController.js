d5f8ba798562d956d15d024f11238127ad08a579
RetiortUK <aauliagazzam@gmail.com>
2025-08-12 14:45:20 +0700

    feat: delete user and patch user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -25,5 +25,44 @@ export const createUser = async (req, res) => {
 };
 
 // PATCH
+export const updateUser = async (req, res) => {
+    try {   
+            if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+                return res.status(400).json({message: "Invalid ID Format"});
+            }
+
+            const updatedUser = await User.findByIdAndUpdate(
+                req.params.id,
+                req.body,
+                {new: true}
+            );
+
+            if (!updatedUser) {
+                return res.status(400).json({message: "User Not Found"});
+            }
+            res.json(updatedUser);
+
+    } catch (err) {
+        return res.status(400).json({message: err.message});
+    }
+};
 
 // DELETE
+export const deleteUser = async (req, res) => {
+    try {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return res.status(400).json({message: "invalid ID Format"});
+        }
+
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+
+        if(!deletedUser) {
+            return res.status(400).json({message: "User Not Found"});
+        }
+
+        res.json({message: "User Deleted Successfully"});
+
+    } catch (err) {
+        return res.status(500).json({message: err.message});
+    }
+};
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getUsers, createUser } from "../controllers/userController.js";
+import { getUsers, createUser, updateUser, deleteUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 // PATH
 router.get("/", getUsers);
 router.post("/", createUser);
+router.patch("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
