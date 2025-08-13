fd8e1124244e2a25e4f2b2936abbf89b42359e4d
Retiortuk <aauliagazzam@gmail.com>
2025-08-13 12:45:06 +0700

    fix: login return actual thing that needed in frontend

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -13,15 +13,25 @@ export const loginUser = async(req, res) => {
         if (!user) return res.status(404).json({message: "User Tidak Ditemukan"});
 
         // Cek Password
-        const isPassworValid = await bcrypt.compare(password, user.password);
-        if (!isPassworValid) return res.status(400).json({message: "Password Salah"});
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) return res.status(400).json({message: "Password Salah"});
 
         const token = jwt.sign(
             {id: user._id, email: user.email, role: user.role},
             process.env.JWT_SECRET,
             {expiresIn: "1d"}
         );
-        return res.status(200).json({message: "Login Berhasil", token});
+        return res.status(200).json({
+            message: "Login Berhasil", 
+            token,
+            user: {
+                id: user._id,
+                namaLengkap: user.namaLengkap,
+                email: user.email,
+                jenisAkun: user.jenisAkun
+            }
+            
+        });
 
     } catch (err) {
         return res.status(500).json({message: err.message});
@@ -42,7 +52,12 @@ export const getUsers = async (req, res) => {
 // POST
 export const createUser = async (req, res) => {
     try {
-        const user =  new User(req.body);
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        const user =  new User({
+            ...req.body,
+            password: hashedPassword
+        });
         const userSaved = await user.save();
         res.status(201).json(userSaved);
 
