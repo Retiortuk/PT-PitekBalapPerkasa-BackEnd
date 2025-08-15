a2faca0ae6ce0120b748cdb4fe587a64b7979027
Retiortuk <aauliagazzam@gmail.com>
2025-08-15 21:14:05 +0700

    Stok now Protected and onlyAdmin

--- Backend/src/routes/stokRoutes.js
+++ Backend/src/routes/stokRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getStoks, createStok, updateStok, deleteStok } from "../controllers/stokController.js";
+import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getStoks);
-router.post("/", createStok);
-router.patch("/:id", updateStok);
-router.delete("/:id", deleteStok);
+router.post("/",protect, isAdmin, createStok);
+router.patch("/:id", protect, isAdmin, updateStok);
+router.delete("/:id", protect, isAdmin, deleteStok);
 
 export default router;
